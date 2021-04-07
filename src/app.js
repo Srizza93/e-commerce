4060471cd1f3626b282773d72b94edd425537440
Simone Rizza <simonerizzanl@gmail.com>
2021-04-07 22:30:05 +0200

    update

--- src/app.js
+++ src/app.js
@@ -1,10 +1,14 @@
+import searchBar from './scripts/search-bar'
+import './styles/search-bar.css'
+
 import navigation from './scripts/navigation';
 import './styles/navigation.css'
 
 import component from './scripts/component';
 import './styles/style.css';
 
-import './image-component';
+// import './image-component';
 
+document.body.appendChild(searchBar());
 document.body.appendChild(navigation());
 document.body.appendChild(component());
--- src/scripts/navigation.js
+++ src/scripts/navigation.js
@@ -1,14 +1,23 @@
 export default () => {
-  const element = document.createElement('div');
+  const navigationBar = document.createElement('div');
   const topics = ['All', `Today's deal`, 'Customer Service', 'Gift Cards', 'Sell', 'Registry'];
-  
-  element.classList.add('container');
+  const icon = document.createElement('div');
+  icon.classList.add('buttons', 'icon');
+
+  for (let i = 0; i < 3; i++) {
+    const bar = document.createElement('div');
+    bar.classList.add('bar');
+    icon.appendChild(bar);
+  }
+
+  navigationBar.appendChild(icon);
+  navigationBar.classList.add('container');
   topics.forEach(topics => {
     const menu = document.createElement('div');
-    element.appendChild(menu);
+    navigationBar.appendChild(menu);
     menu.classList.add('buttons');
     menu.innerHTML = topics;
   })
 
-  return element;
-};
+  return navigationBar;
+}
