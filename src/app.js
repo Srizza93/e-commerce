1b877ecf1618f87b63363d26a13aead862678081
Simone <63718887+Srizza93@users.noreply.github.com>
2021-04-07 22:55:31 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,10 +1,10 @@
-import searchBar from './scripts/search-bar'
+import searchBar from './src/scripts/search-bar'
 import './styles/search-bar.css'
 
-import navigation from './scripts/navigation';
+import navigation from './src/scripts/navigation';
 import './styles/navigation.css'
 
-import component from './scripts/component';
+import component from './src/scripts/component';
 import './styles/style.css';
 
 // import './image-component';
