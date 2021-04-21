1083519eb1fab7145a2f73c2565d704021a66276
Simone <63718887+Srizza93@users.noreply.github.com>
2021-04-21 16:52:10 +0200

    Create App.vue

---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template v-for="id in message">
+  <div class="footer">
+    {{ message.id }}
+  </div>
+</template>
+
+<style scoped src='./styles/footer.css'></style>
+
+<script>
+export default {
+  name: 'App',
+  props: ['Hello'],
+  data() {
+    return {
+      message: {
+        id:'You loaded this page on ' + new Date().toLocaleString()},
+        id: 'hello',
+        id: 'here'
+    }
+  }
+};
+</script>
