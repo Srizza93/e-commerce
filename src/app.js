2f2c11887b22280120d52e3dece7ee91949eecec
Simone <63718887+Srizza93@users.noreply.github.com>
2021-04-21 16:53:32 +0200

    Update app.js

--- src/app.js
+++ src/app.js
@@ -1,3 +1,6 @@
+import Vue from 'vue';
+import App from './App.vue';
+
 import './styles/style.css';
 
 import searchBar from './scripts/search-bar';
@@ -12,9 +15,17 @@ import './styles/sub-nav.css';
 import pageContent from './scripts/page-content';
 import './styles/page-content.css';
 
+import footer from './scripts/footer'
+
 // import './image-component';
 
 document.body.appendChild(searchBar());
 document.body.appendChild(navigation());
 document.body.appendChild(subNav());
 document.body.appendChild(pageContent());
+document.body.appendChild(footer());
+
+new Vue({
+  el: '.footer',
+  render: a => a(App)
+})
