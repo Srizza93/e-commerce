1e6f1c89e46c2d62b80363589e457a461bbc25dd
Simone <63718887+Srizza93@users.noreply.github.com>
2021-04-25 23:00:39 +0200

    Delete src directory

--- src/scripts/sub-nav.js
+++
@@ -1,8 +0,0 @@
-export default () => {
-  const stripe = document.createElement('div');
-  
-  stripe.classList.add('sub-nav');
-  stripe.innerHTML = 'Due to COVID you might experience some delay';
-  
-  return stripe;
-}
